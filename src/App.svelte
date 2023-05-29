00dac66b3b9cf7ca3aa607ed7bfa76fe5a9dd086
Patrick Schaldenbrand <patrickschaldenbrand@gmail.com>
2023-05-29 09:44:06 -0400

    Add remaining files for previous commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,33 @@
 <script>
-  import "carbon-components-svelte/css/g10.css";
-  import { TextArea } from "carbon-components-svelte";
-  import { Button } from "carbon-components-svelte";
-
   let value = "";
 
-  function doSearch() {
-    let response = fetch(
-      "http://localhost:3000/search?" +
+  let artists = "";
+
+  async function doSearch() {
+    let response = await fetch(
+      "http://localhost:8080/Like/Artist?" +
         new URLSearchParams({
-          query: value,
+          search: value,
         })
     );
-    console.log(response);
+    artists = await response.text();
   }
 </script>
 
-<h1>Hello</h1>
-
-<TextArea
-  labelText="App description"
-  placeholder="Enter a description..."
-  bind:value
-/>
-
-<Button on:click={doSearch}>Search</Button>
+<div class="flex">
+  <div class="mx-auto grid bg-gray-100">
+    <h3 class="mx-auto p-10 text-2xl font-bold">Similar Artists</h3>
+    <div class="m-5 grid grid-cols-2 gap-10">
+      <input
+        class="content-center"
+        bind:value
+        placeholder="Enter an artist..."
+      />
+      <button
+        class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+        on:click={doSearch}>Search</button
+      >
+      <ul class="col-span-2">{artists}</ul>
+    </div>
+  </div>
+</div>
